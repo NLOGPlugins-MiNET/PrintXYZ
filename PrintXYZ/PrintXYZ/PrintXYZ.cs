3eb49350a7ed00eaba0947dddd3a22aa6110c7df
박찬솔 <sorisem4106@naver.com>
2017-08-14 20:39:43 +0900

    source file

---
+++ PrintXYZ/PrintXYZ/PrintXYZ.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MiNET;
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+using MiNET.Net;
+using MiNET.Utils;
+using MiNET.Plugins.Commands;
+
+namespace PrintXYZ
+{
+    [Plugin]
+    public class PrintXYZ : Plugin
+    {
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            Console.WriteLine("[PrintXYZ] > Enable!");
+            Console.WriteLine("[PrintXYZ] Made by ANDLOG | 엔로그");
+        }
+        
+        [PacketHandler, Receive]
+        public Package onMove (McpeMovePlayer pk, Player pl)
+        {
+           if(pl.Inventory.GetItemInHand().Id == 345)
+            {
+                double x = Math.Round(pl.KnownPosition.X, 1);
+                double y = Math.Round(pl.KnownPosition.Y, 1);
+                double z = Math.Round(pl.KnownPosition.Z, 1);
+                pl.SendTitle($"§6X : {x} Y : {y} Z : {z}", stayTime: 1);
+            }
+            return pk;
+        }
+    }
+}
